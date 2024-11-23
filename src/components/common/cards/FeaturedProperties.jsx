51051571f7c90cedd2f229edc616e8a83ed18574
femmyte <sanyaoluadefemi@gmail.com>
2024-11-23 10:57:01 +0100

    added fav icon to landing page

--- src/components/common/cards/FeaturedProperties.jsx
+++ src/components/common/cards/FeaturedProperties.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import { Button, Image } from '@nextui-org/react';
 import { MdStar } from 'react-icons/md';
 import { FaUser } from 'react-icons/fa';
 import { FaCalendarDays } from 'react-icons/fa6';
 import NextImage from 'next/image';
+import { MdFavoriteBorder, MdOutlineFavorite } from 'react-icons/md';
+
 const FeaturedProperties = () => {
+	const [isLove, setIsLove] = useState(false);
+
+	const handleSetLove = (e) => {
+		e.preventDefault();
+		setIsLove(!isLove);
+	};
 	return (
 		<div
 			className='flex flex-col gap-[12px] border border-[#F8F9FB] p-4'
@@ -78,18 +87,55 @@ const FeaturedProperties = () => {
 					<p>July16, 2024</p>
 				</div>
 			</div>
-			<div className='flex my-[12px]'>
-				<p className='font-[400] text-[16px] leading-[140%] text-[#323232] '>
-					<span>₦150,000</span> <span>P/A</span>
-				</p>
-				<p className='text-[14px] text-[#323232] leading-[142.857%]'>
-					{' '}
-					· ₦200,000 total Package
-				</p>
+			<div className='flex justify-between items-center'>
+				<div className=''>
+					<div className='flex my-[12px]'>
+						<p className='font-[400] text-[16px] leading-[140%] text-[#323232] '>
+							<span>₦150,000</span> <span>P/A</span>
+						</p>
+						<p className='text-[14px] text-[#323232] leading-[142.857%]'>
+							{' '}
+							· ₦200,000 total Package
+						</p>
+					</div>
+					<p className='font-[400] text-[16px] leading-[140%] text-[#89898A] '>
+						740 total views, 0 today
+					</p>
+				</div>
+				<div className=''>
+					{isLove ? (
+						<div className='flex flex-col items-center justify-center'>
+							<Button
+								isIconOnly
+								variant='faded'
+								aria-label='Like'
+								onClick={handleSetLove}
+								className='border-none bg-none'
+							>
+								<MdFavoriteBorder color='#2C71F6' size={48} />
+							</Button>
+							<p className='font-[500] text-[0.875rem] leading-[140%] text-[#89898A] '>
+								Add to Fav
+							</p>
+						</div>
+					) : (
+						<div className='flex flex-col items-center justify-center'>
+							<Button
+								isIconOnly
+								variant='faded'
+								aria-label='Like'
+								onClick={handleSetLove}
+								className='border-none bg-none'
+							>
+								<MdOutlineFavorite color='#2C71F6' size={48} />
+							</Button>
+							<p className='font-[500] text-[0.875rem] leading-[140%] text-[#89898A] '>
+								Added to Fav
+							</p>
+						</div>
+					)}
+				</div>
 			</div>
-			<p className='font-[400] text-[16px] leading-[140%] text-[#89898A] '>
-				740 total views, 0 today
-			</p>
 		</div>
 	);
 };
